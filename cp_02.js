d0d00dd8328dcce7cf2767baac3938ea97c454ce
Francisco Quinones <fquinonescedeno@usf.edu>
2025-10-18 13:52:27 -0400

    Step 4

--- cp_02.js
+++ cp_02.js
@@ -15,3 +15,18 @@ function fetchProductsThen() {
         handleError(error);
     });
 }
+
+// Step 4 fetchProductsAsync
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products');
+        if(!response.ok) {
+            throw new Error(`Error reported: ${response.status}`);
+        }
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
